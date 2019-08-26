8da242a4a5472c364d70341b802199e853d86201
Rod Ladines <rladines@eki-consulting.com>
2019-08-26 11:20:45 -0500

    Sync up with latest

--- config/EnvSettings.js
+++ config/EnvSettings.js
@@ -1,8 +1,8 @@
 //overrides for this specific environment
 //Defaults are still held in Config.js
 
-ogrid.Config.service.endpoint = 'http://localhost:8080/opengridservice/rest';
-//ogrid.Config.service.endpoint = 'http://opengrid-service-dev-1134290206.us-west-2.elb.amazonaws.com/opengrid-service/rest';
+//ogrid.Config.service.endpoint = 'http://localhost:8080/opengridservice/rest';
+ogrid.Config.service.endpoint = 'https://chicago.opengrid.io/opengrid-service/rest';
 
 //http://chicago.opengrid.io/opengrid-service/rest
 //ogrid.Config.service.endpoint = 'http://chicago.opengrid.io/opengrid-service/rest';
